62e42e3c135b0d68b48693a969bc9f190e3ba877
Aditya101415 <official.aditya8787@gmail.com>
2026-05-31 13:37:50 +0530

    ...

---
+++ NODE.JS/math.js
@@ -0,0 +1,15 @@
+const sum = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+module.exports  = "hello world";
+
+let obj = {
+    sum:sum,
+    subtract:subtract,
+    multiply:multiply
+
+}
+module.exports = obj;
---
+++ NODE.JS/script.js
@@ -0,0 +1,4 @@
+const math = require('./math');
+
+console.log(math);
+console.log(math.sum(2,2));
