82a592d9e56c5a9b5cb1ff0e49dc8ddf837754b7
Aditya101415 <official.aditya8787@gmail.com>
2026-06-03 13:32:58 +0530

    EJS

--- ExpressDir/EJSDir/index.js
+++ ExpressDir/EJSDir/index.js
@@ -7,6 +7,11 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     res.render("home.ejs");
 });
+app.get("/ig/:username", (req, res) => {
+    let {username } = req.params;
+    res.render("instagram.ejs", { username });
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
