4e1ff2041f4e2b195c4c7ba596cf3af601b72b5f
Aditya101415 <official.aditya8787@gmail.com>
2026-06-14 11:00:17 +0530

    added PATCH

--- REST/index.js
+++ REST/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const path = require('path');
+const { randomUUID } = require('crypto');
+const methodOverride = require('method-override');
+
+app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
@@ -14,6 +18,7 @@ app.get("/posts/new", (req, res) => {
 app.post("/posts", (req, res) => {
     let { username, content } = req.body;
     posts.push({
+        id: randomUUID(),
         username,
         content
     });
@@ -22,23 +27,23 @@ app.post("/posts", (req, res) => {
 
 let posts = [
     {
-        id: "1a",
+        id: randomUUID(),
         username: "Adarsh",
         title: "My first post",
         content: "This is my first post on my blog"
     },
     {
-        id: "2b",
+        id: randomUUID(),
         username: "Adarsh",
         title: "My second post",
         content: "This is my second post on my blog"
     },
     {
-        id: "3c",
-    username: "Adarsh",
-    title: "My third post",
-    content: "This is my third post on my blog"
-}
+        id: randomUUID(),
+        username: "Adarsh",
+        title: "My third post",
+        content: "This is my third post on my blog"
+    }
 ];
 
 
@@ -53,5 +58,28 @@ app.listen(port, () => {
 app.get("/posts/:id", (req, res) => {
     let { id } = req.params;
     let post = posts.find(p => p.id === id);
+    if (!post) {
+        return res.status(404).send("Post not found");
+    }
     res.render("show.ejs", { post });
+});
+
+app.patch("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let newContent = req.body.content;
+    let post = posts.find(p => p.id === id);
+    if (!post) {
+        return res.status(404).send("Post not found");
+    }
+    post.content = newContent;
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find(p => p.id === id);
+    if (!post) {
+        return res.status(404).send("Post not found");
+    }
+    res.render("edit.ejs", { post });
 });
