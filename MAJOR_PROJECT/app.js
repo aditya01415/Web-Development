c463252c55e2d108f5f7328410db9d739fe1cd5f
Aditya101415 <official.aditya8787@gmail.com>
2026-06-30 19:33:02 +0530

    ...

--- MAJOR_PROJECT/app.js
+++ MAJOR_PROJECT/app.js
@@ -4,6 +4,7 @@ const app = express();
 const path = require('path');
 const Listing = require('./Models/listing.js');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 main().then(()=>console.log("connected to database")).catch(err=>console.log(err));
 
@@ -15,6 +16,8 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
 
 
 app.get("/",(req,res)=>{
